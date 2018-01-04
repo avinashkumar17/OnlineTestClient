de65052a3a761d16e2e183dc2f15da38dffa855c
naveenn <naveen.nproj@gmail.com>
2018-01-04 19:20:49 +0530

    naveen cha
    nges

--- client/src/main/java/com/example/demo/controller/MyController.java
+++ client/src/main/java/com/example/demo/controller/MyController.java
@@ -3,13 +3,13 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class MyController {
 
-@RequestMapping("login")
-public String index(){
-	return "login";
-}
-	
+	@RequestMapping("login")
+	public String index() {
+		System.out.println("Test ok ");
+		return "login";
+	}
+
 }
