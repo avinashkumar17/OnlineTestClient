aed78834615b74cbe00e80e48e77fdafaa438064
avinashkumar17 <avinashsivakumar17061993@gmail.com>
2018-01-08 19:30:13 +0530

    changes
    done

--- client/src/main/java/com/example/demo/controller/MyController.java
+++ client/src/main/java/com/example/demo/controller/MyController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +14,18 @@ public class MyController {
 		System.out.println("Test ok ");
 		return "login";
 	}
+	@RequestMapping("index")
+	public String index1(Model mode) {
+		System.out.println("Test ok 2");
+		ArrayList<String> s=new ArrayList<String>();
+		s.add("data");
+		mode.addAttribute("dat","hello");	
+		int i =10;
+		float b=(float)i;
+			
+		System.out.println("the "+b);
+		return "index";
+	}	
+	
 
 }
